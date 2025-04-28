95090206cab4d9c0c85dcc73854e02198c26e159
vanshhthakral <vanshnrw@gmail.com>
2025-04-29 00:46:13 +0530

    Day-2 (User input&Variables)

---
+++ Practice set 1/Kilometers.java
@@ -0,0 +1,9 @@
+
+public class Kilometers {
+    public static void main(String[] args) {
+        System.out.println("The distance in miles is : ");
+        int d1 = 2000; 
+        double miles = ((d1)/1.609);
+        System.out.print(miles);
+    }
+}
---
+++ Practice set 1/Percentage.java
@@ -0,0 +1,12 @@
+
+
+public class Percentage {
+    public static void main(String[] args) {
+        System.out.println("The Average marks of student are :");
+        int sub1 = 78;
+        int sub2 = 67;
+        int sub3 = 89;
+        int Average = (sub1 + sub2 + sub3)/3;
+        System.out.print(Average);
+    }
+}
