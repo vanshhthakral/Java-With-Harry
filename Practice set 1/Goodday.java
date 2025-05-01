73b7b9dd793b114a81f513227f4b738246ea33ec
vanshhthakral <vanshnrw@gmail.com>
2025-05-01 17:24:13 +0530

    Final Solutions of Practice set 1

---
+++ Practice set 1/Goodday.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Goodday {
+    public static void main(String[] args) {
+    System.out.println("What is your name");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println("Hello " + name + " Have A good day ");
+
+    }
+}
---
+++ Practice set 1/Integer.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+
+public class Integer {
+    public static void main(String[] args) {
+        System.err.println("Enter your number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
