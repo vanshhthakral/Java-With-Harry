a9c88ccda7890a654a2c6c5eea1a0af0e8ba57d1
vanshhthakral <vanshnrw@gmail.com>
2025-05-02 15:27:45 +0530

    Practice set 3 Solved

---
+++ Practice set 3/Lowercase.java
@@ -0,0 +1,15 @@
+
+
+public class Lowercase {
+    public static void main(String[] args) {
+        String name = new String("Vansh");
+
+
+        String lString = name.toLowerCase();
+        System.out.println(lString);
+                
+            }
+        
+            
+    
+}
---
+++ Practice set 3/Replace.java
@@ -0,0 +1,9 @@
+
+public class Replace {
+    public static void main(String[] args) {
+        String name = new String("Harry Sharma");
+
+        String rString = name.replace(" " , "_");
+        System.out.println(rString);
+    }
+}
