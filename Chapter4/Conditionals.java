ba35da47522f51e85387335fcd20e76613e91972
vanshhthakral <vanshnrw@gmail.com>
2025-05-03 21:30:18 +0530

    Chapter 4 Conditionals

---
+++ Chapter4/Conditionals.java
@@ -0,0 +1,28 @@
+
+
+public class Conditionals {
+       public static void main(String[] args) {
+            int age = 17;
+
+            if (age>18) {
+            System.out.println("Yes boi you are adult");
+            }
+
+            else { 
+             System.out.println("Stay at home little child");    
+            }
+
+            String var = "Vansh";
+
+            switch(var){
+               case "Vansh" :
+               System.out.println("You are good boy ");
+               break;
+            
+               case "Sourabh" :
+               System.out.println("You are not good boy");
+                break;
+               
+            }
+       }
+}
