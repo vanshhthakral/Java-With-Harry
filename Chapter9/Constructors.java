9d2cc6f48d7c1999edee59d7fb68be17b45a93ad
vanshhthakral <vanshnrw@gmail.com>
2025-05-06 17:35:25 +0530

    Chapter 9 Constructors

---
+++ Chapter9/Constructors.java
@@ -0,0 +1,15 @@
+
+        class CWH{
+        CWH(String s, int b){
+    
+            System.out.println("This is the " +b+ "th video of "+ " "+ s);
+        }
+    }
+    
+    public class Constructors {
+        public static void main(String[] args) {
+            CWH obj1 = new CWH("CodeWithHarry Java Playlist",42);
+    
+        }
+    }
+
